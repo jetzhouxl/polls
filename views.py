ca24afb569844f256611f7e3164d16382521477f
xiaolongzhou <34395750+jetzhouxl@users.noreply.github.com>
2017-12-11 17:42:49 +0800

    完成了教程4
    实现表单内容

---
+++ views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import loader
+from .models import Question,Choice
+from django.http import Http404
+from django.urls import reverse
+# Create your views here.
+def index(request):
+    latest_question_list=Question.objects.order_by('-pub_date')[:5]
+    template=loader.get_template('polls/index.html')
+    #所谓的上下文环境就是传入模板里面的参数，一定要参数名对应
+    context={
+        #由变量名加对象组成的字典
+        'latest_question_list':latest_question_list,
+    }
+    return HttpResponse(template.render(context,request))
+
+def detail(request,question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request,question_id):
+    question=get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
+def vote(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
+
+
+
+
+
+
+
+
